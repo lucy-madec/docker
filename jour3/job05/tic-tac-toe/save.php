b7908b37b1e7bd3ca7f4e84e5ba31fd37d23d90e
Lucy <lucy.madec@laplateforme.io>
2025-02-17 11:19:27 +0100

    Delete the 'jour3' directory with all files in to restart the tic-tac-toe exercices

--- jour3/job05/tic-tac-toe/save.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $data = json_decode(file_get_contents('php://input'), true);
-    file_put_contents('results.json', json_encode($data['board']));
-    echo 'Game saved!';
-}
-?>
